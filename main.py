685b243cfdccac854dcf78ec157eef675c6c23e2
Guillaume-4 <guillaume.blas4@gmail.com>
2025-10-18 18:06:46 +0200

    Adding FindAllSolution functions for the 8 queens problems

--- main.py
+++ main.py
@@ -6,17 +6,18 @@ import random
 nb_pop = 25
 select_hcount = 5
 select_lcount = 5
-
-    # Don't touch this
-list_pop = []
-solution = False
-nb_gen = 1
 BOARD_SIZE= 8
+
+    # /!\ Don't touch this /!\
+list_pop = []
+list_all_solution = []
+nb_gen = 1
 RDM_SELECT = select_lcount + select_hcount -1
+
 
 ''' FUNCTION '''
 
-def fillPop(pop ,count: int):
+def fillPop(pop: list[object] ,count: int):
     """
         Refills `pop` with random Individuals while length of `pop` < `count`
     """
@@ -30,13 +31,13 @@ def create_random_pop(count: int):
     for _ in range(count):
         list_pop.append(Individual(random.sample(range(BOARD_SIZE),BOARD_SIZE)))
 
-def evaluate(pop: list):
+def evaluate(pop: list[object]):
     """
         Sorts the `pop` with each `nbconflict`
     """
     pop.sort(key=lambda individu: individu.nbconflict)
 
-def selection(pop: list, hcount: int, lcount: int):
+def selection(pop: list[object], hcount: int, lcount: int):
     """
         Keeps `hcount` best Individuals of the `pop` list and the `lcount` worst
     """
@@ -63,6 +64,50 @@ def mutation(ind: object):
     nb_rdm = random.randint(0,7)
     ind.list[index_rdm] = nb_rdm
 
+def checkSolution(allSolution : list[object], solutionToCheck: object):
+    """
+        Checks if a `solutionToCheck` is in `allSolution` list
+    """
+    for i in allSolution:
+        if(i.list == solutionToCheck.list):
+            return True
+    return False
+
+
+
+def findSolution(messages: bool):
+    """
+        Found a solution for the 8 queens problems with the `pop`
+    """
+    global nb_gen, nb_pop, list_pop
+    list_pop.clear()
+    create_random_pop(nb_pop)
+    while(True):
+        if messages:
+            print(f"Attempt#{nb_gen}")
+        evaluate(list_pop)
+        if(list_pop[0].nbconflict == 0):
+            if messages:
+                print(f"{list_pop[0]} \nIn {nb_gen} generation(s)")
+            return list_pop[0]
+        else:
+            selection(list_pop,select_hcount,select_lcount)
+            new_generation(list_pop[random.randint(0,RDM_SELECT)], list_pop[random.randint(0,RDM_SELECT)])
+            mutation(list_pop[random.randint(0,RDM_SELECT)])
+            fillPop(list_pop, nb_pop)
+        nb_gen += 1
+
+
+def findAllSolution():
+    """
+        Looking for all solution of 8 queens problems 
+    """
+    global list_all_solution, nb_pop
+    while(True):
+        print(f"Numbers of solutions find : {len(list_all_solution)}")
+        temp_solution = findSolution(False)
+        if not checkSolution(list_all_solution, temp_solution):
+            list_all_solution.append(temp_solution)
 
 ''' CLASSE '''
 
@@ -72,7 +117,7 @@ class Individual:
         For each column, indicate the row where the queen is located.
     """
     
-    def __init__(self, lst: list):
+    def __init__(self, lst: list[int]):
         """
             Constructor of the `Individu` class
         """
@@ -112,20 +157,14 @@ class Individual:
 
 ''' MAIN PROGRAM '''   
 
-create_random_pop(nb_pop)
+"""
+    If you want to have all solutions for the 8 queens problems,
+    just write : 
+        findAllSolution()
 
+    If you need just one write : 
+        findSolution(True) 
+"""
 
-while(not solution):
-    print(f"Attempt#{nb_gen}")
-    evaluate(list_pop)
-    if(list_pop[0].nbconflict == 0):
-        solution = True
-        print(f"{list_pop[0]} \nIn {nb_gen} generation(s)")
-    else:
-        selection(list_pop,select_hcount,select_lcount)
-        new_generation(list_pop[random.randint(0,RDM_SELECT)], list_pop[random.randint(0,RDM_SELECT)])
-        mutation(list_pop[random.randint(0,RDM_SELECT)])
-        fillPop(list_pop, nb_pop)
-    nb_gen += 1
-        
-    
+findAllSolution()
+#findSolution(True)
